19900e6dd4dfe9b96f98b08e981bc4315491576c
Ryan Szemplinski <rszemplinski22@gmail.com>
2019-12-01 17:43:54 -0500

    Making some more changes to the installation scripts

--- ClientService/Nodegem.ClientService/AppConstants.cs
+++ ClientService/Nodegem.ClientService/AppConstants.cs
@@ -2,7 +2,7 @@ namespace Nodegem.ClientService
 {
     public static class AppConstants
     {
-        public const string NodegemEndpoint = "https://nodester-staging.herokuapp.com";
+        public const string NodegemEndpoint = "https://nodegem-heroku-api.herokuapp.com";
         public const int PingTime = 15000;
     }
 }
